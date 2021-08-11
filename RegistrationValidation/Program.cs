2f6a4c8f22b66d65538849380831c1943b2c1f2d
pavanyalala <pavanyalala4508@gmail.com>
2021-08-11 21:29:57 +0530

    [pavan]:Welcome message added

---
+++ RegistrationValidation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegistrationValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+        }
+    }
+}
