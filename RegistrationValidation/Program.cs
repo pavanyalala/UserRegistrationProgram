1f063761d57c309495ec8e77aad42c6df31d555a
pavanyalala <pavanyalala4508@gmail.com>
2021-08-11 21:48:06 +0530

    [pavan]:First Name Validation Added

--- RegistrationValidation/Program.cs
+++ RegistrationValidation/Program.cs
@@ -7,6 +7,9 @@ namespace RegistrationValidation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+            UserRegister userRegister = new UserRegister();
+            bool val =  userRegister.FirstNameValidate("Pavan");
+            Console.WriteLine(val);
         }
     }
 }
---
+++ RegistrationValidation/UserRegister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegistrationValidation
+{
+    class UserRegister
+    {
+        string FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        /// <summary>
+        /// first name validation
+        /// </summary>
+        /// <returns></returns>
+        public bool FirstNameValidate(string name)
+        {
+           
+            Regex regex = new Regex(FirstName);
+            bool Validation = regex.IsMatch(name);
+            if(Validation)
+                Console.WriteLine("First Name is Valid");
+            else
+                Console.WriteLine("First Name is Not Valid");
+            return Validation;
+        }
+        
+    }
+}
